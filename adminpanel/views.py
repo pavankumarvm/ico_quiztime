45930ef83250aed2e7a04493c3fbb03ba31807ae
abhikoreti <abhinavkoreti727@gmail.com>
2022-08-27 11:38:59 +0530

    leaderboard page

--- adminpanel/views.py
+++ adminpanel/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
   return render(request, template_name='index.html')
+  
+def home(request):
+  return render(request, template_name='home.html')
+  
+  
