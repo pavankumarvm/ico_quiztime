9e4465d55b020d6827756207c55c0cadc0445dc3
pavankumarvm <pavankumarmaurya1999@gmail.com>
2022-08-27 17:31:14 +0530

    Sidebar included

--- accounts/admin.py
+++ accounts/admin.py
@@ -9,15 +9,15 @@ from .models import IcoUser
 
 class UserAdmin(UserAdmin):
 
-    ordering = ('email',)
-    list_display = ('email', 'is_admin',)
-    search_fields = ('email','first_name')
+    ordering = ('username', 'email',)
+    list_display = ('username', 'email', 'is_admin',)
+    search_fields = ('username', 'email','first_name')
     readonly_fields = ('date_joined', 'last_login',)
     
     filter_horizontal = ()
     list_filter = ()
     fieldsets = (
-        (None,{'fields': ('email', 'password')}),
+        (None,{'fields': ('username', 'email', 'password')}),
         ('Personal Information',{'fields' : ('first_name', 'last_name',)}),
         ('Permissions',{'fields': ('is_staff', 'is_admin', 'is_superuser', 'is_active')}),
         ('Important Dates', {'fields': ('date_joined', 'last_login')}),
