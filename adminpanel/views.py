070b29988fd058b11090bde6187875da35e2c6c3
Shani Dev Yadav <yadavshani67@gmail.com>
2022-08-27 12:58:24 +0530

    profile page

--- adminpanel/views.py
+++ adminpanel/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
   return render(request, template_name='index.html')
+
+def profile(request):
+  return render(request, template_name='profile.html')
   
 def leaderboard(request):
   return render(request, template_name='leaderboard.html')
