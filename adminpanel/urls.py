0c11809a450ba2978e40b3388e46e9f52c488bed
pavankumarvm <pavankumarmaurya1999@gmail.com>
2022-08-27 21:53:44 +0530

    Files created

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -19,9 +19,15 @@ from adminpanel.views import *
 
 urlpatterns = [
   path('', index, name='index'),
-  path('profile/', profile, name='profile'),
-  path('dashboard/', dashboard, name='dashboard'),
-  path('rules/', rules ,name='rules'),  
-  path('leaderboard/',leaderboard,name='leaderboard'),
   path('adminpanel/', adminpanel, name='adminpanel'),
+  path('adminpanel/create_quiz/', create_quiz, name='adminpanel'),
+  path('adminpanel/edit_quiz/', edit_quiz, name='adminpanel'),
+  path('adminpanel/delete_quiz/', delete_quiz, name='adminpanel'),
+  path('adminpanel/newadmin', add_new_admin, name='add_new_admin')
+  # User APIS
+  path('user/dashboard/', dashboard, name='dashboard'),
+  path('user/leaderboard/', leaderboard, name='leaderboard'),
+  path('user/personal_scores/', personal_scores, name='personal_scores'),
+  path('user/rules/',user_rules,name='user_rules'),
+  path('user/take_quiz/', take_quiz, name='take_quiz'),
 ]
--- adminpanel/views.py
+++ adminpanel/views.py
@@ -4,17 +4,32 @@ from django.shortcuts import render
 def index(request):
   return render(request, template_name='index.html')
 
-def profile(request):
-  return render(request, template_name='profile.html')
-
-def dashboard(request):
-  return render(request, template_name='dashboard.html')
-  
-def leaderboard(request):
-  return render(request, template_name='leaderboard.html')
-
 def adminpanel(request):
   return render(request, template_name='adminpanel.html')
 
-def rules(request):
-  return render(request, template_name='rules.html')
+def create_quiz(request):
+  return render(request, template_name='create_quiz.html')
+
+def edit_quiz(request):
+  return render(request, template_name='edit_quiz.html')
+
+def delete_quiz(request):
+  return render(request, template_name='delete_quiz.html')
+
+def add_new_admin(request):
+  return render(request, template_name='newadmin.html')
+
+def dashboard(request):
+  return render(request, template_name='dashboard.html')
+
+def leaderboard(request):
+  return render(request, template_name='leaderboard.html')
+
+def personal_scores(request):
+  return render(request, template_name='personal_scores.html')
+
+def user_rules(request):
+  return render(request, template_name='user_rules.html')
+
+def take_quiz(request):
+  return render(request, template_name='take_quiz.html')
