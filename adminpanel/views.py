438691708fa5aeeb8e8977a23b3f24d294d0a189
abhikoreti <43955206+abhikoreti@users.noreply.github.com>
2022-08-27 12:48:42 +0530

    leaderboard page (#1)
    * leaderboard page
    
    * Changes done
    
    Co-authored-by: Pavankumar Maurya <54816032+pavankumarvm@users.noreply.github.com>

--- adminpanel/views.py
+++ adminpanel/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
   return render(request, template_name='index.html')
+  
+def leaderboard(request):
+  return render(request, template_name='leaderboard.html')
 
 def adminpanel(request):
   return render(request, template_name='adminpanel.html')
